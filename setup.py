5219cf29d9ce6828b4f7fdddc4183e129fee9e77
Hannes Krumbiegel <Vuizur@users.noreply.github.com>
2023-09-10 12:52:46 +0200

    Add setuptools to requirements

--- setup.py
+++ setup.py
@@ -30,7 +30,8 @@ py_version = sys.version_info[:2]
 install_requires = [
     'dawg-python >= 0.7.1',
     'docopt >= 0.6',
-    'pymorphy3-dicts-ru'
+    'pymorphy3-dicts-ru',
+    'setuptools'
 ]
 if py_version < (3, 0):
     install_requires.append("backports.functools_lru_cache >= 1.0.1")
